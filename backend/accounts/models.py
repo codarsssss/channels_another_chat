6dbc9b0ef2d34f1128c4254ff16c6f5459606556
codarsssss <codarsssss@gmail.com>
2024-02-13 17:49:28 +0300

    запуск, создание пользователя, установка poetry

---
+++ backend/accounts/models.py
@@ -0,0 +1,32 @@
+from django.core.validators import RegexValidator
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.utils import timezone
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=255,unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    created = models.DateTimeField(default=timezone.now)
+    phone_regex = RegexValidator(regex=r'^((\+7)|8)\d{10}$',
+                                 message="Неверный формат номера телефона")
+    phone_number = models.CharField(validators=[phone_regex], max_length=12,
+                                    null=True, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
